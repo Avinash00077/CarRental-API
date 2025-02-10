1332a7b8584881988cfca2cee3d8eb8b5e81ba0d
Avinash Reddy <avinashreddytummuri77@gmail.com>
2025-02-10 22:39:35 +0530

    slots response changes

--- services/booking.service.js
+++ services/booking.service.js
@@ -142,7 +142,12 @@ const GetAvilableSlotsService = async (request) => {
   try {
     const { start_date, start_time, end_date, end_time, car_id } = request.headers;
     const data = await BookingDTO.GetAvilableSlotsDTO(start_date, start_time, end_date, end_time, car_id);
-    return data;
+    const groupedResults = data.reduce((acc, { available_date, time_slot }) => {
+      if (!acc[available_date]) acc[available_date] = [];
+      acc[available_date].push(time_slot);
+      return acc;
+  }, {});
+    return groupedResults;
   } catch (error) {
     logger.error({ GetAvilableSlotsService: error.message });
     throw new Error(error.message);
@@ -153,7 +158,12 @@ const GetAvilableSlotsByStartDateService = async (request) => {
   try {
     const { start_date, start_time, car_id } = request.headers;
     const data = await BookingDTO.GetAvilableSlotsByStartDateDTO(start_date, start_time, car_id);
-    return data;
+    const groupedResults = data.reduce((acc, { available_date, time_slot }) => {
+      if (!acc[available_date]) acc[available_date] = [];
+      acc[available_date].push(time_slot);
+      return acc;
+  }, {});
+    return groupedResults;
   } catch (error) {
     logger.error({ GetAvilableSlotsByStartDateService: error.message });
     throw new Error(error.message);
